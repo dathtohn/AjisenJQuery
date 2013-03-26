d2aee261a56fbe6a7e04b0c6d59f80a7e085dfa8
Eric Liu <dathtohn@gmail.com>
2013-03-25 22:41:39 -0400

    Rename

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '6e006e21c24d4cf7b87cee454e58cf2976c54bf53201a73f7f9073d87ba581db300137ba0381119c9b8c27be22702a339ad1f7a5395bfd0d7461e2090bd531a1'
+Ajisen::Application.config.secret_token = '6e006e21c24d4cf7b87cee454e58cf2976c54bf53201a73f7f9073d87ba581db300137ba0381119c9b8c27be22702a339ad1f7a5395bfd0d7461e2090bd531a1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-FirstApp::Application.routes.draw do
+Ajisen::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
