eb78c245cfbb687e2f8ee17ff3003958953b67e7
Eric Liu <dathtohn@gmail.com>
2013-03-30 16:08:18 -0400

    update

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -1,7 +1,12 @@
-
+/*
 $(document).ready(function() {
 	
 	var currentPage = "home";
+	//$("#content").animate({ width: "toggle"}, "slow");
+	
+	$("#mainbar").slideDown(1200, function(){
+		$("#homebtn-container").slideDown("slow");
+	});
 	
 	//change the background
 	var changeBg = function(oldPage, newPage) {
@@ -12,12 +17,12 @@ $(document).ready(function() {
 	//move mainbar and toggle content div
 	var moveMainbar = function(oldPage, newPage) {
 		if (oldPage == "home") {
-			$("#mainbar").animate({"left": "25%"}, 1000);
-			$("#content").animate({width: 'toggle'}, 1000);
+			$("#mainbar").animate({ "left": "25%" }, 1000);
+			//$("#content").animate({ width: "show"}, 1000);
 		}
 		else if (newPage == "home") {
-			$("#content").animate({width: 'toggle'}, 1000);
-			$("#mainbar").animate({"left": "50%"}, 1000)
+			//$("#content").animate({ width: "hide" }, 1000);
+			$("#mainbar").animate({ "left": "50%" }, 1000);
 		}
 	};
 	
