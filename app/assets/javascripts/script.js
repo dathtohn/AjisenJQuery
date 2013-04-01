d690df7ccb22a3a1c4302226ccc5c8ee8987444f
Eric Liu <dathtohn@gmail.com>
2013-04-01 17:10:55 -0400

    content update

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -17,18 +17,37 @@ $(document).ready(function() {
 	//move mainbar and toggle content div
 	var moveMainbar = function(oldPage, newPage) {
 		if (oldPage == "home") {
-			$("#mainbar").animate({ "left": "25%" }, 1000);
+			$("#mainbar").animate({ "left": "25%" }, 600);
 			$("#content").animate({ width: "show"}, 2000);
 		}
 		else if (newPage == "home") {
-			$("#content").animate({ width: "hide" }, 1000);
-			$("#mainbar").animate({ "left": "50%" }, 1000);
+			$("#content").animate({ width: "hide" }, 800);
+			$("#mainbar").animate({ "left": "50%" }, 1200);
 		}
 	};
+	
+	var changeLink = function(oldPage, newPage) {
+		$("#" + oldPage + "btn").css( "color", "#FFF" );
+		$("#" + oldPage + "btn").mouseover(function() {
+			$("#" + oldPage + "btn").css( "color", "#CC0000" );
+		});
+		$("#" + oldPage + "btn").mouseleave(function() {
+			$("#" + oldPage + "btn").css( "color", "#FFF" );
+		});
+		
+		$("#" + newPage + "btn").css( "color", "#CC0000" );
+		$("#" + newPage + "btn").mouseover(function() {
+			$("#" + newPage + "btn").css( "color", "#CC0000" );
+		});
+		$("#" + newPage + "btn").mouseleave(function() {
+			$("#" + newPage + "btn").css( "color", "#CC0000" );
+		});
+	}
 	
 	//home link
 	$("#homebtn").click(function() {
 		if (currentPage != "home") {
+			changeLink(currentPage, "home");
 			moveMainbar(currentPage, "home");
 			changeBg(currentPage, "home");
 			currentPage = "home";
@@ -38,6 +57,7 @@ $(document).ready(function() {
 	//locations link
 	$("#locationsbtn").click(function() {
 		if (currentPage != "locations") {
+			changeLink(currentPage, "locations");
 			moveMainbar(currentPage, "locations");
 			changeBg(currentPage, "locations");
 			currentPage = "locations";
@@ -47,6 +67,7 @@ $(document).ready(function() {
 	//menu link
 	$("#menubtn").click(function() {
 		if (currentPage != "menu") {
+			changeLink(currentPage, "menu");
 			moveMainbar(currentPage, "menu");
 			changeBg(currentPage, "menu");
 			currentPage = "menu";
